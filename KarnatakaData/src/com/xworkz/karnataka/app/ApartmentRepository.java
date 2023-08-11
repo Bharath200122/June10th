a21a6ff6466c563df0893bbad073fe59b56b6727
Bharathhy007 <bharathhy6@gmail.com>
2023-08-11 11:55:05 +0530

    Data Storing and Displaying by using abstract

---
+++ KarnatakaData/src/com/xworkz/karnataka/app/ApartmentRepository.java
@@ -0,0 +1,13 @@
+
+
+package com.xworkz.karnataka.app;
+
+public interface ApartmentRepository {
+	int TOTAL_APARTMENTS=5;
+	String name(String nameOfApartment);
+	default void display()
+	{
+
+	}
+
+}
---
+++ KarnatakaData/src/com/xworkz/karnataka/app/ArmyNamesRepository.java
@@ -0,0 +1,12 @@
+
+
+package com.xworkz.karnataka.app;
+
+public interface ArmyNamesRepository {
+	int TOTAL_NAMES=5;
+	String army(String armyNames);
+	default void print()
+	{
+
+	}
+}
